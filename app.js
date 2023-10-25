523caec5c7f701e0c6ffac95c75343cdeca68343
Esteban Lopez <brandonlopezmonroy@gmail.com>
2023-10-24 20:49:47 -0500

    prueba deploy

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app = express();
 mongoose.connect(
   'mongodb://drenvio:moM5f3AodwLE5d0A@ac-aemgtkt-shard-00-00.unqyghm.mongodb.net:27017,ac-aemgtkt-shard-00-01.unqyghm.mongodb.net:27017,ac-aemgtkt-shard-00-02.unqyghm.mongodb.net:27017/challenge?replicaSet=atlas-y8oxsk-shard-0&ssl=true&authSource=admin'
 );
+
+app.get('/', async (req, res) => {
+  res.send("Funciona correctamente");
+});
+
   
   app.get('/products', async (req, res) => {
     const products = await mongoose.connection.db.collection('products');  
